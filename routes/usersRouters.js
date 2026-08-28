5088dec42f12ff561c315a1d5c69df3c9e5cbabf
Sess254 <wamaritemoses@gmail.com>
2026-08-28 11:42:41 +0300

    rollback

--- routes/usersRouters.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/usersController');
-
-router.get('/sign-up', userController.signUpFormGet);
-router.post('/sign-up', userController.signUpPost);
-
-module.exports = router;
